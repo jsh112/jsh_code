b6f89e1407d4610a13210a705fbcb17f4e70e33b
jsh112 <jshkr1120@gmail.com>
2025-09-30 17:40:47 +0900

    change color

--- JSH_Climbing.py
+++ JSH_Climbing.py
@@ -368,7 +368,7 @@ def main():
 
     # ================= 색상 필터 선택 =================
     # 하드코딩 예: 전체 표시
-    selected_class_name  = 'Hold_Green'     
+    selected_class_name  = 'Hold_Yellow'     
     selected_color_label = "all"
     csv_label = _sanitize_label(selected_color_label)
     print(f"[Info] Color filter: {selected_color_label}")
@@ -414,7 +414,8 @@ def main():
     # 1. 좌표 추출
     ptsL = np.array([idxL[hid]["center"] for hid in common_ids], dtype=np.float32)
     ptsR = np.array([idxR[hid]["center"] for hid in common_ids], dtype=np.float32)
-
+    print(f"ptsL is {ptsL}")
+    print(f"ptsR is {ptsR}")
     # 2. Fundamental matrix 계산 (테스트용)
     # 좌우 수평 오차 계산 (Fundamental matrix 없이)
     y_errors = np.abs(ptsL[:,1] - ptsR[:,1])
@@ -460,7 +461,7 @@ def main():
     
 
     # ===== Servo 초기화 & 초기 조준 =====
-    ctl = DualServoController() if not HAS_SERVO else DualServoController(args.port, args.baud)
+    # ctl = DualServoController() if not HAS_SERVO else DualServoController(args.port, args.baud)
     current_target_id = route_ids[0]
     mr0 = by_id[current_target_id]
     yaw_cmd0, pitch_cmd0 = to_servo_cmd(mr0["yaw_deg"], mr0["pitch_deg"])
@@ -529,7 +530,7 @@ def main():
                             dyaw, dpitch = delta_from_id[tid]
                             target_yaw   = cur_yaw - dyaw
                             target_pitch = cur_pitch + dpitch
-                            send_servo_angles(ctl, target_yaw, target_pitch)
+                            # send_servo_angles(ctl, target_yaw, target_pitch)
                             cur_yaw, cur_pitch = target_yaw, target_pitch
                             current_target_id = next_id_map[tid]
                             last_advanced_time = now_t
@@ -550,7 +551,7 @@ def main():
         cv2.destroyAllWindows()
         try: pose.close()
         except: pass
-        try: ctl.close()
+        try: print("hi")# ctl.close()
         except: pass
 if __name__ == "__main__":
     main()
